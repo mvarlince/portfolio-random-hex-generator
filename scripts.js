a3ca8e6bb967ea8f619325237331f7bc47d059a9
Varlince Meran <varlincemeran@gmail.com>
2023-02-01 14:13:12 -0500

    random hex color generator

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function getRandomColor(){
+    const hexColorCharacter = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F']
+    let color = '#'
+    for (let i = 0; i < 6; i++){
+        color += hexColorCharacter[Math.floor(Math.random() * hexColorCharacter.length)]
+    }
+    return color
+}
+
+function renderRandomColor(){
+    const currentRandomColor = getRandomColor()
+    document.getElementById("htmlBody").style.backgroundColor=currentRandomColor
+    document.getElementById("htmlH1").innerHTML = currentRandomColor
+    console.log(currentRandomColor)
+}
+
+renderRandomColor()
